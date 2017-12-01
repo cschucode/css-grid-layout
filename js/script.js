821a32305eae9b09cbb095c4d35559184167a56a
Chris Schuhmacher <cschuhmacher@fandom.com>
2017-12-01 14:06:44 -0800

    Cards getting reinserted based on browser width

--- js/script.js
+++ js/script.js
@@ -2,35 +2,45 @@ $(document).ready(() => {
 	// check browser width to render feed cards
 	checkBrowserWidth()
 
+	// displays content based on size of the browser
 	$(window).resize(function() {
 		checkBrowserWidth();
 	});
 
 });
 
-const moveCards = (cards, destination) => {
-	$(cards).appendTo(destination);
-}
-
+// checks browser width and postitions cards accordingly
 const checkBrowserWidth = () => {
 	let mediaQueryListTablet = window.matchMedia("(max-width: 1023px)").matches;
 	let mediaQueryListDesktop = window.matchMedia("(min-width: 1024px) and (max-width: 1279px)");
 	let mediaQueryListXL = window.matchMedia("(min-width: 1280px)");
 
 	if (mediaQueryListTablet) {
-		// reinsertCards('.right-rail .feed-interrupt');
+		reinsertCards();
 	}
 
 	if (mediaQueryListDesktop.matches) {
 		moveCards('.feed-interrupt', '.right-rail');
-		// reinsertCards('.left-rail .social-card');
 	}
+
 	if (mediaQueryListXL.matches) {
-		moveCards('.social-card', '.left-rail');
-		moveCards('.feed-interrupt', '.right-rail');
+		moveCards('.social-card', '.right-rail');
+		moveCards('.feed-interrupt', '.left-rail');
 	}
 }
 
-const reinsertCards = (cards) => {
-	// code goes here :)
+// moves feed cards into place
+const moveCards = (cards, destination) => {
+	$(cards).appendTo(destination);
+}
+
+// should reinsert cards back into main feed
+const reinsertCards = () => {
+	const $cards = $('.feed');
+
+	const $sorted = $cards.sort(function(a, b) {
+		return $(a).data('order') - $(b).data('order');
+	});
+
+	$('.main').append($sorted);
 }
